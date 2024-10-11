795d43003a612c2781a16f96c4235fe4905478a1
TEACHER\ICT02_22 <admin@admin.com>
2024-10-11 12:16:11 +0900

    로또 완성

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./../App";
 import Footer from "../components/layout/Footer";
 import LottoBall from "../components/lotto/LottoBall";
+import LottoPage from "../components/pages/LottoPage";
 
 const routes = [
   {
@@ -12,7 +13,7 @@ const routes = [
       {
         path: "/",
         loader: () => "로또",
-        element: <LottoBall />,
+        element: <LottoPage />,
       },
     ],
   },
