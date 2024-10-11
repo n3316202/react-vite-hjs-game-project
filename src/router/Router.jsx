ae5382cd8e61885f0b5b1758315cf2ddb72145e9
TEACHER\ICT02_22 <admin@admin.com>
2024-10-11 14:29:22 +0900

    commit

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,7 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./../App";
-import Footer from "../components/layout/Footer";
-import LottoBall from "../components/lotto/LottoBall";
 import LottoPage from "../components/pages/LottoPage";
 
 const routes = [
