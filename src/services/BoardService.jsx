6242c69b9209365e2b1eeb85da11b27018429b3f
TEACHER\ICT02_22 <admin@admin.com>
2024-10-15 11:16:10 +0900

    commit

--- src/services/BoardService.jsx
+++ src/services/BoardService.jsx
@@ -4,6 +4,12 @@ const getPagingList = (path = "/boards/list", search = "") => {
   return http.get(path + search);
 };
 
+http://192.168.0.10:8282/boards/$64
+const remove = (id) => {
+  return http.delete(`boards/${id}`);
+};
+
 export default {
   getPagingList,
+  remove,
 };
