9d6df08edb52b6ce8c9fcaa53f81dd383da68478
사용자 이름 < >
2025-02-27 17:49:48 +0900

    commit

--- src/components/board/Pagingnation.jsx
+++ src/components/board/Pagingnation.jsx
@@ -2,7 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 //https://getbootstrap.com/docs/5.0/components/pagination/
-
+//    /* 생성자 */
+    public PageMakerDTO(Criteria cri, int total) {
+        
+        this.cri = cri;
+        this.total = total;
+        
+        /* 마지막 페이지 */
+        this.endPage = (int)(Math.ceil(cri.getPageNum()/10.0))*10;
+        /* 시작 페이지 */
+        this.startPage = this.endPage - 9;
+        
+        /* 전체 마지막 페이지 */
+        int realEnd = (int)(Math.ceil(total * 1.0/cri.getAmount()));
+        
+        /* 전체 마지막 페이지(realend)가 화면에 보이는 마지막페이지(endPage)보다 작은 경우, 보이는 페이지(endPage) 값 조정 */
+        if(realEnd < this.endPage) {
+            this.endPage = realEnd;
+        }
+        
+        /* 시작 페이지(startPage)값이 1보다 큰 경우 true */
+        this.prev = this.startPage > 1;
+        
+        /* 마지막 페이지(endPage)값이 1보다 큰 경우 true */
+        this.next = this.endPage < realEnd;
+        
+        
+    }
 const Pagingnation = ({ paging, onClickPaging }) => {
   console.log(paging);
 
